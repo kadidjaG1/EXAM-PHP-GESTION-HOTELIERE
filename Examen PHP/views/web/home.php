a06da8a4f672c778e668719508dd9328c7d1404d
kadidjaG1 <nana-kadidjatou.iro@ism.edu.sn>
2026-07-09 18:11:50 +0000

    Delete Examen PHP directory

--- Examen PHP/views/web/home.php
+++
@@ -1,9 +0,0 @@
-<h2>Bienvenue</h2>
-<p>Utilisez le menu ci-dessus pour gérer les réservations de l'hôtel :</p>
-<ul>
-    <li>Créer une nouvelle réservation</li>
-    <li>Consulter les réservations actives</li>
-    <li>Annuler une réservation</li>
-    <li>Consulter le chiffre d'affaires prévisionnel</li>
-    <li>Consulter le séjour le plus long</li>
-</ul>
